b510b9e1aa4cd7f78ff8d58b2b08f6062363631d
Michael Ries <michael@riesd.com>
2014-08-15 23:38:36 -0600

    remove extra logging and simplify javascript

--- web/channels/songs_channel.ex
+++ web/channels/songs_channel.ex
@@ -6,13 +6,7 @@ defmodule Playa.SongsChannel do
   end
 
   def event(socket, "play", %{"url" => url}=message) do
-    IO.puts("play! #{url}")
     broadcast socket, "play", message
-    socket
-  end
-  def event(socket,event,message) do
-    IO.inspect(message)
-    IO.puts("unknown event: #{event}")
     socket
   end
 end
